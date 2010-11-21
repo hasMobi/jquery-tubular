30b433a5d21e1a2b9dd245c2789a69930447bc9c
seanmccambridgedesign <seanmccambridgedesign@a784bb9d-31bc-ed5d-9f76-821d591c1af0>
2010-11-21 06:59:59 +0000

    Up and running.

---
+++ js/jquery.tubular.js
@@ -0,0 +1,42 @@
+// set params
+var videoWidth = 853;
+var videoRatio = 16/9; // either 4/3 or 16/9
+
+jQuery.fn.tubular = function(videoId) {
+	t = setTimeout("resizePlayer()",1000);
+	jQuery('html,body').css('height','100%');
+	jQuery('body').prepend('<div id="yt-container" style="overflow: hidden; position: absolute; z-index: 1;"><div id="ytapiplayer">You need Flash player 8+ and JavaScript enabled to view this video.</div></div>');
+	
+	// initiailize vars
+	var ytplayer = 0;
+	var pageWidth = 0;
+	var pageHeight = 0;
+	var videoHeight = videoWidth / videoRatio;
+	var duration;
+	
+	// swfobject embed yt player
+	var params = { allowScriptAccess: "always", wmode: "transparent" };
+	var atts = { id: "myytplayer" };
+	swfobject.embedSWF("http://www.youtube.com/v/" + videoId + "?enablejsapi=1&playerapiid=ytplayer&loop=1", "ytapiplayer", videoWidth, videoHeight, "8", null, null, params, atts);
+	
+	jQuery(window).resize(function() {
+		resizePlayer();
+	});
+}
+
+// player calls this function on ready
+function onYouTubePlayerReady(playerId) {
+	ytplayer = document.getElementById("myytplayer");
+	ytplayer.setPlaybackQuality('medium');
+	ytplayer.mute();
+}
+
+function resizePlayer() {
+	var newWidth = jQuery(window).width(); // original page width
+	var newHeight = jQuery(window).height(); // original page height
+	jQuery('#yt-container').width(newWidth).height(newHeight);
+	if (newHeight > newWidth / videoRatio) { // if window ratio becomes taller than video
+		newWidth = newHeight * videoRatio; // overflow video to sides instead of bottom
+	}
+	jQuery('#myytplayer').width(newWidth).height(newWidth/videoRatio);
+}
