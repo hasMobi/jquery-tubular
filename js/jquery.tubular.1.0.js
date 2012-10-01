9c3caca2640b0e39bd0ccb931853b7d8eff82668
seanmccambridgedesign <seanmccambridgedesign@gmail.com>
2012-10-01 20:22:56 +0000

    rebuilt tubular for iframe API using better practices. back to original functionality. more to come.

---
+++ js/jquery.tubular.1.0.js
@@ -0,0 +1,122 @@
+/* jQuery tubular plugin
+|* by Sean McCambridge
+|* http://www.seanmccambridge.com/tubular
+|* since 2011
+|* licensed under the MIT License
+|* Enjoy.
+|* 
+|* Thanks,
+|* Sean */
+
+;(function ($, window) {
+
+    // test for feature support and return if failure
+    
+    // defaults
+    var defaults = {
+        ratio: 16/9, // usually either 4/3 or 16/9 -- tweak as needed
+        videoId: 'ZCAnLxRvNNc', // toy robot in space is a good default, no?
+        mute: true,
+        repeat: true,
+        width: $(window).width(),
+        wrapperZIndex: 99,
+        playButtonClass: 'tubular-play',
+        pauseButtonClass: 'tubular-pause',
+        muteButtonClass: 'tubular-mute'
+    };
+
+    // methods
+
+    var tubular = function(node, options) { // should be called on the wrapper div
+        var options = $.extend({}, defaults, options),
+            $body = $('body') // cache body node
+            $node = $(node); // cache wrapper node
+
+        // build container
+        var tubularContainer = '<div id="tubular-container" style="overflow: hidden; position: fixed; z-index: 1; width: 100%; height: 100%"><div id="tubular-player" style="position: absolute"></div></div><div id="tubular-shield" style="width: 100%; height: 100%; z-index: 2; position: absolute;"></div>';
+
+        // set up css prereq's, inject tubular container and set up wrapper defaults
+        $('html,body').css({'width': '100%', 'height': '100%'});
+        $body.prepend(tubularContainer);
+        $node.css({position: 'relative', 'z-index': options.wrapperZIndex});
+
+        // set up iframe player, use global scope so YT api can talk
+        window.player;
+        window.onYouTubeIframeAPIReady = function() {
+            player = new YT.Player('tubular-player', {
+                width: options.width,
+                height: Math.ceil(options.width / options.ratio),
+                videoId: options.videoId,
+                events: {
+                    'onReady': onPlayerReady,
+                    'onStateChange': onPlayerStateChange
+                }
+            });
+        }
+
+        window.onPlayerReady = function(e) {
+            resize();
+            if (options.mute) e.target.mute();
+            e.target.playVideo();
+        }
+
+        window.onPlayerStateChange = function() {
+            console.log('state change');
+        }
+
+        // resize handler updates width, height and offset of player after resize/init
+        var resize = function() {
+            var width = $(window).width(),
+                pWidth, // player width, to be defined
+                height = $(window).height(),
+                pHeight, // player height, tbd
+                $tubularPlayer = $('#tubular-player');
+
+            // when screen aspect ratio differs from video, video must center and underlay one dimension
+
+            if (width / options.ratio < height) { // if new video height < window height (gap underneath)
+                pWidth = Math.ceil(height * options.ratio); // get new player width
+                $tubularPlayer.width(pWidth).height(height).css({left: (width - pWidth) / 2, top: 0}); // player width is greater, offset left; reset top
+            } else { // new video width < window width (gap to right)
+                pHeight = Math.ceil(width / options.ratio); // get new player height
+                $tubularPlayer.width(width).height(pHeight).css({left: 0, top: (height - pHeight) / 2}); // player height is greater, offset top; reset left
+            }
+
+        }
+
+        // events
+        $(window).on('resize.tubular', function() {
+            resize();
+        })
+
+        $('body').on('click','.' + options.playButtonClass, function(e) {
+            e.preventDefault();
+            player.playVideo();
+        }).on('click', '.' + options.pauseButtonClass, function(e) {
+            e.preventDefault();
+            player.pauseVideo();
+        }).on('click', '.' + options.muteButtonClass, function(e) {
+            e.preventDefault();
+            (player.isMuted()) ? player.unMute() : player.mute();
+        })
+    }
+
+    // load yt iframe js api
+
+    var tag = document.createElement('script');
+    tag.src = "//www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // create plugin
+
+    $.fn.tubular = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'tubular_instantiated')) {
+                $.data(this, 'tubular_instantiated', 
+                tubular(this, options));
+            }
+        });
+    }
+
+})(jQuery, window);
