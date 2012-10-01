c7fe021a8d14ca9bc658e560ff97e9a281e80c1d
seanmccambridgedesign <seanmccambridgedesign@gmail.com>
2012-10-01 21:59:28 +0000

    some content and option updates

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	var options = {  };
+	var options = { videoId: 'e4Is32W-ppk' };
 	$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
